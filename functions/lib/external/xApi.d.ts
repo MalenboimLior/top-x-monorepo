26732e86bfce30bb1a465dfdcaaa5e59373fee74
HedoniaBuild <qa1@hedonia.io>
2025-08-25 16:08:42 +0300

    more

--- functions/lib/external/xApi.d.ts
+++ functions/lib/external/xApi.d.ts
@@ -1,3 +1,7 @@
 export declare const postOnX: import("firebase-functions/v2/https").CallableFunction<{
     text: string;
+    media?: {
+        data: string;
+        mimeType: string;
+    }[];
 }, any, unknown>;
